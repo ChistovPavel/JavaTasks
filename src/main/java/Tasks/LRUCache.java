e4445cb87cfc294b49f59781226f486def1cd87c
ChistovPavel <pavlik.9947@gmail.com>
2020-05-30 02:28:13 +0300

    LRUCache O(N) and LRUCache O(1) added

---
+++ src/main/java/Tasks/LRUCache.java
@@ -0,0 +1,55 @@
+package Tasks;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class LRUCache
+{
+    private HashMap<Integer, Integer> map;
+    private LinkedList<Integer> list;
+    private Integer capacity;
+
+    public LRUCache(int inCapacity) {
+        this.map = new HashMap<Integer, Integer>(inCapacity+1, 1);
+        this.list = new LinkedList<Integer>();
+        this.capacity = inCapacity;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        list.remove((Object)key);
+        list.addLast(key);
+        return map.get(key);
+    }
+
+    public void put(int key, int value)
+    {
+        if (map.size() == capacity)
+        {
+            if (!map.containsKey(key))
+            {
+                map.remove(this.list.getFirst());
+                list.removeFirst();
+                list.addLast(key);
+            }
+            else
+            {
+                list.remove((Object)key);
+                list.addLast(key);
+            }
+        }
+        else
+        {
+            if (!map.containsKey(key))
+            {
+                list.addLast(key);
+            }
+            else
+            {
+                list.remove((Object)key);
+                list.addLast(key);
+            }
+        }
+        map.put(key, value);
+    }
+}
---
+++ src/main/java/Tasks/LRUCacheN.java
@@ -0,0 +1,46 @@
+package Tasks;
+
+import java.util.LinkedHashMap;
+
+public class LRUCacheN
+{
+    private LinkedHashMap<Integer, Integer> map;
+    private Integer capacity;
+
+    public LRUCacheN(int inCapacity) {
+        this.map = new LinkedHashMap<Integer, Integer>(inCapacity+1, 1);
+        this.capacity = inCapacity;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+        int value = map.get(key);
+        map.remove(key);
+        map.put(key, value);
+        return value;
+    }
+
+    public void put(int key, int value)
+    {
+        if (map.size() == capacity)
+        {
+            if (!map.containsKey(key))
+            {
+                map.remove(map.keySet().iterator().next());
+            }
+            else
+            {
+                map.remove(key);
+            }
+        }
+        else
+        {
+            if (map.containsKey(key))
+            {
+                map.remove(key);
+                map.put(key, value);
+            }
+        }
+        map.put(key, value);
+    }
+}
